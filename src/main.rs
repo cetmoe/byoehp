0000881988355b84f7d50caf21846050e0ca7ac3
cetmoe <6438758+cetmoe@users.noreply.github.com>
2024-08-16 00:52:17 +0200

    hehe

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use ntex::{
-    http::{body, Request},
-    util::HashMap,
-    web,
-};
+use ntex::web;
 use reqwest::{self, header::HeaderValue};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[web::get("/")]
 async fn hello() -> impl web::Responder {
@@ -30,13 +28,28 @@ async fn fetch_profile(player_name: web::types::Path<String>) -> impl web::Respo
             web::HttpResponse::Ok().body(body)
         }
         Err(_) => {
-            web::HttpResponse::NoContent().body(format!("HTTP error while fetching profile.",))
+            web::HttpResponse::NoContent().body(format!("HTTP error while fetching profile."))
         }
     }
 }
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
+    for v in std::env::vars() {
+        println!("{:?}", v);
+    }
+    const DB_KEY: &str = "pgsql_db";
+    let connection_string = env::var(DB_KEY)
+        .expect(format!("Database connection string not found. {}", DB_KEY).as_str());
+
+    println!("Connection string: {}", connection_string);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(22)
+        .connect(&connection_string.to_owned())
+        .await
+        .expect("Failed to connect to database.");
+
     web::HttpServer::new(|| web::App::new().service(fetch_profile))
         .bind(("127.0.0.1", 8080))?
         .run()
