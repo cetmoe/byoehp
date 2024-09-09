54d9e047e6aebed468e0da76ed5e1ac1a7ccf4ee
cetmoe <6438758+cetmoe@users.noreply.github.com>
2024-09-09 23:28:02 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,49 @@
-use ntex::web;
+use dotenv::dotenv;
 use reqwest::{self, header::HeaderValue};
-use sqlx::postgres::PgPoolOptions;
+use serde_json::{json, Value};
 use std::env;
+use tokio::net::TcpListener;
 
-#[web::get("/")]
-async fn hello() -> impl web::Responder {
-    web::HttpResponse::Ok().body("Hello world!")
-}
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
 
-#[web::get("/profile/{username}")]
-async fn fetch_profile(player_name: web::types::Path<String>) -> impl web::Responder {
+const X: &str = "https://apps.runescape.com/runemetrics/profile/profile?user=mowws";
+
+async fn fetch_profile(Path(name): Path<String>) -> Json<Value> {
     let url = format!(
         "https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player={}",
-        player_name
+        name
     );
 
-    match reqwest::get(url).await {
+    match reqwest::get(url.clone()).await {
         Ok(request) => {
             if request.headers().get("content-type")
                 != Some(&HeaderValue::from_static("application/json"))
             {
-                return web::HttpResponse::ExpectationFailed()
-                    .body(format!("Invalid content type."));
+                return Json(json!({"Error": 32, "hehe": url.clone()}));
             }
 
-            let body = request.text().await.unwrap();
-            web::HttpResponse::Ok().body(body)
+            let body = request.json().await.unwrap();
+            Json(body)
         }
-        Err(_) => {
-            web::HttpResponse::NoContent().body(format!("HTTP error while fetching profile."))
-        }
+        Err(_) => Json(json!({"Error": 42})),
     }
 }
 
-#[ntex::main]
-async fn main() -> std::io::Result<()> {
-    for v in std::env::vars() {
-        println!("{:?}", v);
-    }
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
     const DB_KEY: &str = "pgsql_db";
     let connection_string = env::var(DB_KEY)
         .expect(format!("Database connection string not found. {}", DB_KEY).as_str());
 
-    println!("Connection string: {}", connection_string);
+    let app = Router::new().route("/profile/:character_name", get(fetch_profile));
 
-    let pool = PgPoolOptions::new()
-        .max_connections(22)
-        .connect(&connection_string.to_owned())
-        .await
-        .expect("Failed to connect to database.");
-
-    web::HttpServer::new(|| web::App::new().service(fetch_profile))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
